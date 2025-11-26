57d86246bb070cbf6e012f11d4842e871fe663b6
Pavan <yengalwarpavan@gmail.com>
2025-11-26 12:16:00 +0530

    comming the comment

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Login from './Components/login';
 import Home from './Components/Home';
 
 function App() {
+  //App folder structure
+  
   return (
     <div className="App">
       <Router>
